69116e0b248a02675cc44466984df43e4000a293
Radhika Thorat <radhikathorat28194@outlook.com>
2026-07-29 11:25:32 +0530

    To update code for CRUD operation

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Asp.Versioning;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using ProductApplication.DTOs;
 
 namespace ProductAPI.Controllers
 {
@@ -25,19 +26,63 @@ namespace ProductAPI.Controllers
             var items = await _service.GetAllAsync();
             return Ok(items);
         }
-        /// <summary>
-        /// Returns specfic product.
-        /// </summary>
-        /// <returns>product details</returns>
 
-        [HttpGet("{id}")]
+        /// <summary>
+        /// Returns a product by id.
+        /// </summary>
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById(int id)
         {
             var item = await _service.GetByIdAsync(id);
             if (item == null) return NotFound();
             return Ok(item);
-        }      
-        
-       
+        }
+
+        /// <summary>
+        /// Creates a new product.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDto dto)
+        {
+            var result = await _service.CreateAsync(dto);
+
+            return CreatedAtAction(nameof(GetById),
+                new { id = result.Id, version = "1.0" },
+                result);
+        }
+
+        /// <summary>
+        /// Updates an existing product.
+        /// </summary>
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, UpdateProductDto dto)
+        {
+            await _service.UpdateAsync(id, dto);
+
+            return Ok(new
+            {
+                Message = "Product updated successfully."
+            });
+        }
+
+        /// <summary>
+        /// Deletes a product.
+        /// </summary>
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+
+            return Ok(new
+            {
+                Message = "Product deleted successfully."
+            });
+        }
+
+
+
+
+
+
     }
 }
